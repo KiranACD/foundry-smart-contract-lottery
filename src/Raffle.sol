80dd3a5b36a87f27c4d9ef173951e9b5c04c3e24
Kiran <kiran.ram@gmail.com>
2026-01-14 00:22:39 +0000

    Raffle is VRFConsumer

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,29 +1,90 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity 0.8.19;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 /**
  * @title A sample Raffle contract
  * @author Kiran Ramachandran
  * @notice This contract is for creating a sample raffle
  * @dev It implements Chainlink VRF2.5 and chainlink automation
  */
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
+    /** Errors */
+    error SendMoreToEnterRaffle();
 
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
-    constructor(uint256 entranceFee) {
+    event RaffleEntered(address indexed player);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
+        i_interval = interval;
         i_entranceFee = entranceFee;
+        s_lastTimeStamp = block.timestamp;
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
-        
+        if (msg.value < i_entranceFee) {
+            revert SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+
+        // 1. Makes migration easier
+        // 2. Makes frontend indexing easier
+        emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {}
+    // Generate a random number
+    // Use random number to pick a player
+    // Call function automatically
+    function pickWinner() public {
+        // Chek if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            // Add a custom error
+            revert();
+        }
+
+        uint256 s_requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
+            })
+        );
+    }
 
     /** Getter Function */
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal virtual override {
+        
     }
 }
